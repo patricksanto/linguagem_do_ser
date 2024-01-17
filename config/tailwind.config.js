1f37e9ca155cb437797bb85c9a4f677c2fa9478d
Patrick Santos <patrick.sg.santos@gmail.com>
2024-01-17 07:08:45 -0800

    add tailwind to the project and create the first controller and view

---
+++ config/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Noto Serif', 'serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
