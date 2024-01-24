d0471cba4a5f082f2a66df6bf8e938a81b87b0c5
Patrick Santos <patrick.sg.santos@gmail.com>
2024-01-24 14:05:47 -0800

    added the front end and tailwind classes to almost all landing page , only need to add the about and do the respnosivity

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,6 +15,25 @@ module.exports = {
         open: ['Open Sans', 'sans-serif'],
       },
     },
+    colors: {
+      serpurple: {
+        200: '#C0C1DB',
+        400: '#928CAF',
+        900: '#695D68',
+      },
+      serblack: {
+        900: '#261523',
+      },
+      serblue: {
+        200: '#E4F0F3',
+        400: '#CCDEE6',
+      },
+      serpink: {
+        200: '#F4E4EF',
+        400: '#E2C5D3',
+        900: '#FF7CCF',
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
